4f933635199844d44e4c4fc91d82c19bd0702a6b
dpizar <diegopizarroribera@gmail.com>
2021-11-29 21:01:20 -0500

    fixed backend

--- run-modules-local/backend.tf
+++ run-modules-local/backend.tf
@@ -7,10 +7,6 @@ terraform {
   backend "gcs" {
     bucket      = "dpizar_terraform_state"
     prefix      = "terraform/local-module-state"
-<<<<<<< HEAD
-    credentials = "../common/auth/terraform-svc.json"
-=======
     credentials = "./auth/terraform-svc.json"
->>>>>>> 079ab9d (Changes for running terraform in docker container)
   }
 }
