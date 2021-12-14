79724a0e6070be4923193a7868dea1af54d626d9
dpizar <diegopizarroribera@gmail.com>
2021-12-13 21:20:14 -0500

    fix: copy terraform files to /terraforfiles/

--- docker-autodeployGKE/gke.sh
+++ docker-autodeployGKE/gke.sh
@@ -2,6 +2,7 @@
 git clone https://github.com/dpizar/dpizar_gke-infrastructure-terraform.git
 
 cd dpizar_gke-infrastructure-terraform/run-modules/
+cp . /terraformfiles/
 
 terraform init
 terraform apply -auto-approve
