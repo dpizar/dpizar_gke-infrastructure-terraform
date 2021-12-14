71a82d253458142eadc0a3012de5af5641905002
dpizar <diegopizarroribera@gmail.com>
2021-12-13 21:29:56 -0500

    more changes

--- docker-autodeployGKE/gke.sh
+++ docker-autodeployGKE/gke.sh
@@ -2,7 +2,7 @@
 git clone https://github.com/dpizar/dpizar_gke-infrastructure-terraform.git
 
 cd dpizar_gke-infrastructure-terraform/run-modules/
-cp -r dpizar_gke-infrastructure-terraform/run-modules/ /terraformfiles/
+cp -r /terraformfiles/dpizar_gke-infrastructure-terraform/run-modules/ /terraformfiles/
 
 cd /terraformfiles/
 
