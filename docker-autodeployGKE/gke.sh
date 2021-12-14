cfb660d05b69262d4d5df2eac5aba40d30092e9b
Diego Pizarro <diegopizarroribera@gmail.com>
2021-12-13 21:27:51 -0500

    Update gke.sh

--- docker-autodeployGKE/gke.sh
+++ docker-autodeployGKE/gke.sh
@@ -2,7 +2,7 @@
 git clone https://github.com/dpizar/dpizar_gke-infrastructure-terraform.git
 
 cd dpizar_gke-infrastructure-terraform/run-modules/
-cp -r . /terraformfiles/
+cp -r dpizar_gke-infrastructure-terraform/run-modules/ /terraformfiles/
 
 cd /terraformfiles/
 
